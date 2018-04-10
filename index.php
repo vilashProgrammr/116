6172abefc93ce202be88a0954562e3e0ff16a0ac
dj+1 <dj+1@programmr.com>
2018-04-10 10:58:07 +0000

    FINISH TEST

--- index.php
+++ index.php
@@ -5,6 +5,11 @@ include_once 'dbconfig.php';
 if(isset($_GET['delete_id']))
 {
 	//delete logic here
+	//this is the test having screen recorder enabled
+	$sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
+	mysql_query($sql_query);
+	header("Location: $_SERVER[PHP_SELF]");
+	//TIME TO FINISH
 	
 }
 // delete condition
