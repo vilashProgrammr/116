0e24bbbda36da927a423f93f42df1d853407724a
dj+8@programmr.com <dj+8@programmr.com>
2018-06-01 11:41:19 +0000

    FINISH TEST

--- index.php
+++ index.php
@@ -7,8 +7,10 @@ if(isset($_GET['delete_id']))
 	//delete logic here
 	//other changes
 	//1434334653654336543
+//one motre
 }
 // delete condition
+
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
